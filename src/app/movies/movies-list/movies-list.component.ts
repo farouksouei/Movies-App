82b2e7c88b40ce7c41982366d6115f6ca204f774
Mohamed Farouk Souei <farouksouei@gmail.com>
2022-12-20 16:28:30 +0100

    props

--- src/app/movies/movies-list/movies-list.component.ts
+++ src/app/movies/movies-list/movies-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-movies-list',
@@ -7,27 +7,8 @@ import { Component } from '@angular/core';
 })
 export class MoviesListComponent {
   ngOnInit(): void {
-    setTimeout(() => {
-      this.movies = [{
-        title: 'The Matrix',
-        year: 1999,
-        rating: 8.7,
-        date: new Date(),
-        price: 9.99,
-      }, {
-        title: 'The Matrix Reloaded',
-        year: 2003,
-        rating: 7.2,
-        date: new Date(),
-        price: 9.99,
-      }, {
-        title: 'The Matrix Revolutions',
-        year: 2003,
-        rating: 6.7,
-        date: new Date(),
-        price: 9.99,
-      }];
-    },3000);
   }
+
+  @Input()
   movies:any;
 }
