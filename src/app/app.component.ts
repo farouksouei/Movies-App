d49f87bc9572228986208f6a193f7e301e5c973e
Mohamed Farouk Souei <farouksouei@gmail.com>
2022-12-20 13:47:24 +0100

    ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.movies = []
+      //   title: 'The Matrix',
+      //   year: 1999,
+      //   rating: 8.7,
+      //   date: new Date(),
+      //   price: 9.99,
+      // }, {
+      //   title: 'The Matrix Reloaded',
+      //   year: 2003,
+      //   rating: 7.2,
+      //   date: new Date(),
+      //   price: 9.99,
+      // }, {
+      //   title: 'The Matrix Revolutions',
+      //   year: 2003,
+      //   rating: 6.7,
+      //   date: new Date(),
+      //   price: 9.99,
+      // }];
+    },3000);
+  }
   title = 'app';
-  movies = [{
-    title: 'The Matrix',
-    year: 1999,
-    rating: 8.7,
-    date: new Date(),
-    price: 9.99,
-  }, {
-    title: 'The Matrix Reloaded',
-    year: 2003,
-    rating: 7.2,
-    date: new Date(),
-    price: 9.99,
-  },];
+  movies:any;
   duplicateNumber(number: number): number {
     return number * 2;
   }
