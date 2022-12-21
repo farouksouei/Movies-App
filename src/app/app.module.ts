d1ab849d4e35b0e1eab50fca4fd24cf2ffe6fb28
Mohamed Farouk Souei <farouksouei@gmail.com>
2022-12-21 14:20:49 +0100

    rate selection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { GenericListComponent } from './utilities/generic-list/generic-list.comp
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { MenuComponent } from './menu/menu.component';
+import { RatingComponent } from './utilities/rating/rating.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MoviesListComponent,
     GenericListComponent,
-    MenuComponent
+    MenuComponent,
+    RatingComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/utilities/rating/rating.component.ts
@@ -0,0 +1,41 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-rating',
+  templateUrl: './rating.component.html',
+  styleUrls: ['./rating.component.css']
+})
+export class RatingComponent {
+
+  @Input()
+  rating: number = 5;
+
+  @Input()
+  selected = 0;
+  previousSelected = 0;
+  maxRatingArr:any = [];
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.maxRatingArr = Array(this.rating).fill(0);
+  }
+
+  handleMouseEnter(index: number) {
+    this.selected = index + 1;
+  }
+
+  handleMouseLeave(){
+    this.selected = 0;
+    if (this.previousSelected !== 0) {
+      this.selected = this.previousSelected;
+    } else {
+    this.selected = 0;
+    }
+  }
+
+  rate(index: number) {
+    this.selected = index + 1;
+    this.previousSelected = this.selected;
+  }
+}
